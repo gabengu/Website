59fc449251ef94134d4deffaa6e6c8754168a5ab
gabengu <gabengu@gmail.com>
2023-01-20 07:50:32 -0700

    navbar changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,17 @@ import image from "../assets/angles-down-solid.svg"
 export default function NavBar(){
     return(
         <nav className = "nav">
-            <h1 className = "nav--title">Gabe Ngu</h1>
-            <img src={image} className = "drop"/>
+            <div className = "nav--before">
+                <h1 className = "nav--title">Gabe Ngu</h1>
+                <img src={image} className = "drop"/>
+            </div>
+            <div className = "links--container">
+                <div className = "links">
+                    <h1>Github</h1>
+                    <h1>poop</h1>
+                    <h1>poop</h1>
+                </div>
+            </div>
         </nav>
     )
 }
