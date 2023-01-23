b20f63ea2437341221c4916ca59e144cfae467ff
gabengu <gabengu@gmail.com>
2023-01-23 00:15:01 -0700

    Started mobile navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,15 @@
-import image from "../assets/angles-down-solid.svg"
+import drop from "../assets/angles-down-solid.svg"
+import lines from "../assets/bars-solid.svg"
 
 export default function NavBar(){
     return(
         <nav className = "nav">
             <div className = "nav--before">
                 <h1 className = "nav--title">Gabe Ngu</h1>
-                <img src={image} className = "drop"/>
+                <img src={drop} className = "drop"/>
+                <img src={lines} className = "lines"/>
             </div>
+            {/* for desktop */}
             <div className = "links--container">
                 <div className = "links">
                     <a className = "link--text" href="https://github.com/gabengu" target="_blank">Github</a>
---
+++ src/components/NavMobile.jsx
@@ -0,0 +1,5 @@
+<div className = "nav--after">
+    <a className = "link--mobile" href="https://github.com/gabengu" target="_blank">Github</a>
+    <a className = "link--mobile" href="#">Email</a>
+    <a className = "link--mobile" href="#">LinkedIn</a>
+</div>
