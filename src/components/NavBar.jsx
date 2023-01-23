0972107aef52a6272c97adfaff3f7223c075423f
gabengu <gabengu@gmail.com>
2023-01-23 00:59:13 -0700

    added mobile navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,21 @@
 import drop from "../assets/angles-down-solid.svg"
 import lines from "../assets/bars-solid.svg"
+import NavMobile from "./NavMobile"
+import { useState } from "react"
 
 export default function NavBar(){
+    const [navOpen, setNavOpen] = useState(false);
+    function clickHandle(){
+        setNavOpen(() => !navOpen)
+    }
     return(
         <nav className = "nav">
             <div className = "nav--before">
                 <h1 className = "nav--title">Gabe Ngu</h1>
                 <img src={drop} className = "drop"/>
-                <img src={lines} className = "lines"/>
+                <img src={lines} className = "lines" onClick={clickHandle}/>
             </div>
+            {navOpen && <NavMobile />}
             {/* for desktop */}
             <div className = "links--container">
                 <div className = "links">
--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,5 +1,11 @@
-<div className = "nav--after">
-    <a className = "link--mobile" href="https://github.com/gabengu" target="_blank">Github</a>
-    <a className = "link--mobile" href="#">Email</a>
-    <a className = "link--mobile" href="#">LinkedIn</a>
-</div>
+export default function NavMobile(){
+    return(
+        <div className = "nav--after">
+            <div className = "mobile--container">
+                <a className = "link--mobile" href="https://github.com/gabengu" target="_blank">Github</a>
+                <a className = "link--mobile" href="#">Email</a>
+                <a className = "link--mobile" href="#">LinkedIn</a>
+            </div>
+        </div>
+    )
+}
