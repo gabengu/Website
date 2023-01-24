19d2181cbdbd419488f1588c15ee7937c5824e02
gabengu <gabengu@gmail.com>
2023-01-23 17:59:01 -0700

    rebuild

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ cd dist
 echo > .nojekyll
 
 # if you are deploying to a custom domain
-echo 'https://gabengu.me' > CNAME
+# echo 'https://gabengu.me' > CNAME
 
 git init
 git checkout -B main
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:gabengu/Website.git main:gh-pages
 
 cd -
